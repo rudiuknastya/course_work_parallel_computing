78536b1d497b55cf64c6865deccde5179af8627a
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-12-20 00:08:44 +0200

    feat(inverted index) - created inverted index and added method to add word and file into it

---
+++ src/InvertedIndex.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class InvertedIndex {
+    private Map<String, Set<String>> map = new HashMap<>();
+    public void addWordAndFile(String word, String filePath){
+        if(map.containsKey(word)){
+            map.get(word).add(filePath);
+        } else {
+            Set<String> files = new HashSet<>();
+            files.add(filePath);
+            map.put(word,files);
+        }
+    }
+}
